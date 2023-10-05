37a3a976636e2ca64f0b5f0bc3d180d8fd00a48f
Polina <polianna_654@mail.ru>
2023-10-05 13:06:45 +0300

    commit

--- task_2/server.js
+++ task_2/server.js
@@ -5,10 +5,30 @@ import fetch from ('node-fetch');
 const PORT = 5000;
 const app = express(); //Сервер
 
-app.post('/api', (req,res)=>{
+app.post('/api',async (req,res)=>{
     try {
-        const {fromValute, outValute, number} = req.body
-        if(fromValute==""||outValute==""||number=="") return res.status(500).json({message: "Не все поля заполнены"})
+        const {fromValute, toValute, number} = req.body
+        if(fromValute==""||toValute==""||number=="") return res.status(500).json({message: "Не все поля заполнены"})
+
+        const apiUrl = "Тут URL зфпроса на сервер"
+
+        //Отправка запроса на сервер для получения текущего курса валюты
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.error) {
+            return res.status(500).json({message: "Что-то пошло не так"})
+          } else {
+            const exchangeRate = data.rates[to];
+      
+            if (exchangeRate) {
+            
+                return res.status(201).json(number * exchangeRate)
+            } else {
+                res.status(error).json({message: "Что-то пошло не так"})
+            }
+          }
+
      
 
     } catch (error) {
